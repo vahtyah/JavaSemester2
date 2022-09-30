9face94fa9e4fd64659583cdcde6277d661f1063
8268826 <8268826@gmail.com>
2022-09-30 14:09:15 +0300

    gcvbcv

--- src/lab_12/lab_12.java
+++ src/lab_12/lab_12.java
@@ -1,22 +1,22 @@
-package src.lab_12;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.geom.Rectangle2D;
-
-public class lab_12 extends JFrame {
-    Shape shape = new Rectangle(20, 30);
-    private CustomShape renderShape;
-
-
-    public lab_12(){
-        super("Task_12");
-        setSize(1000,1000);
-        setLayout(new BorderLayout());
-        renderShape.setShape(shape);
-    }
-
-    public static void main(String[] args) {
-        new lab_12().setVisible(true);
-    }
-}
+//package src.lab_12;
+//
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.geom.Rectangle2D;
+//
+//public class lab_12 extends JFrame {
+//    Shape shape = new Rectangle(20, 30);
+//    private CustomShape renderShape;
+//
+//
+//    public lab_12(){
+//        super("Task_12");
+//        setSize(1000,1000);
+//        setLayout(new BorderLayout());
+//        renderShape.setShape(shape);
+//    }
+//
+//    public static void main(String[] args) {
+//        new lab_12().setVisible(true);
+//    }
+//}
